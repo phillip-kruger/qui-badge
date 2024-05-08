e16f85c7756eaf60c1c57d5d416ba0c6aefb5e35
Phillip Kruger <phillip.kruger@gmail.com>
2024-05-08 17:22:00 +1000

    Initial migration
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,113 @@
 # qui-badge
-Badge component
+
+The badge UI component is based on the Vaadin themed badge (https://vaadin.com/docs/latest/components/badge), but it's a webcomponent
+
+## Installation
+
+```bash
+npm i @qomponent/qui-badge
+```
+
+## Usage
+
+```javascript
+import 'qui-badge';
+```
+
+```html
+<div class="cards">
+    <qui-card title="Small">
+        <div slot="content">
+            <div class="cardcontents">
+                <qui-badge small><span>Default</span></qui-badge>
+                <qui-badge level="success" small><span>Success</span></qui-badge>
+                <qui-badge level="warning" small><span>Warning</span></qui-badge>
+                <qui-badge level="error" small><span>Error</span></qui-badge>
+                <qui-badge level="contrast" small><span>Contrast</span></qui-badge>
+                <qui-badge background="pink" color="purple" small><span>Custom colors</span></qui-badge>
+            </div>
+        </div>
+    </qui-card> 
+    <qui-card title="Primary">
+        <div slot="content">
+            <div class="cardcontents">
+                <qui-badge primary><span>Default primary</span></qui-badge>
+                <qui-badge level="success" primary><span>Success primary</span></qui-badge>
+                <qui-badge level="warning" primary><span>Warning primary</span></qui-badge>
+                <qui-badge level="error" primary><span>Error primary</span></qui-badge>
+                <qui-badge level="contrast" primary><span>Contrast primary</span></qui-badge>
+                <qui-badge background="pink" color="purple" primary><span>Custom colors</span></qui-badge>
+            </div>
+        </div>
+    </qui-card>
+    <qui-card title="Pill">
+        <div slot="content">
+            <div class="cardcontents">
+                <qui-badge pill><span>Default pill</span></qui-badge>
+                <qui-badge level="success" pill><span>Success pill</span></qui-badge>
+                <qui-badge level="warning" pill><span>Warning pill</span></qui-badge>
+                <qui-badge level="error" pill><span>Error pill</span></qui-badge>
+                <qui-badge level="contrast" pill><span>Contrast pill</span></qui-badge>
+                <qui-badge background="pink" color="purple" pill><span>Custom colors</span></qui-badge>
+            </div>
+        </div>
+    </qui-card>
+    <qui-card title="With icon">
+        <div slot="content">            
+            <div class="cardcontents">
+                <qui-badge text="Default" icon="info-circle">
+                    <span>Default icon</span>
+                </qui-badge>
+                <qui-badge text="Success" level="success" icon="vaadin:check-circle">
+                    <span>Success icon</span>
+                </qui-badge>
+                <qui-badge text="Warning" level="warning" icon="vaadin:warning">
+                    <span>Warning icon</span>
+                </qui-badge>
+                <qui-badge text="Error" level="error" icon="vaadin:exclamation-circle">
+                    <span>Error icon</span>
+                </qui-badge>
+                <qui-badge text="Contrast" level="contrast" icon="vaadin:adjust">
+                    <span>Contrast icon</span>
+                </qui-badge>
+                <qui-badge text="Custom" background="pink" color="purple" icon="vaadin:flag-checkered">
+                    <span>Custom colors</span>
+                </qui-badge>
+            </div>
+        </div>
+    </qui-card>
+    <qui-card title="Icon only">
+        <div slot="content">
+            <div class="cardcontents">
+                <qui-badge icon="vaadin:info-circle"></qui-badge>
+                <qui-badge level="success" icon="vaadin:check-circle"></qui-badge>
+                <qui-badge level="warning" icon="vaadin:warning"></qui-badge>
+                <qui-badge level="error" icon="vaadin:exclamation-circle"></qui-badge>
+                <qui-badge level="contrast" icon="vaadin:adjust"></qui-badge>
+                <qui-badge level="contrast" background="pink" color="purple" icon="vaadin:flag-checkered"></qui-badge>
+            </div>
+        </div>
+    </qui-card>
+    <qui-card title="Clickable">
+        <div slot="content">
+            <div class="cardcontents">
+                <qui-badge clickable @click=${() => this._info()}><span>Default</span></qui-badge>
+                <qui-badge clickable level="success" @click=${() => this._success()}><span>Success</span></qui-badge>
+                <qui-badge clickable level="warning" @click=${() => this._warning()}><span>Warning</span></qui-badge>
+                <qui-badge clickable level="error" @click=${() => this._error()}><span>Error</span></qui-badge>
+                <qui-badge clickable level="contrast" @click=${() => this._contrast()}><span>Contrast</span></qui-badge>
+                <qui-badge clickable background="pink" color="purple" @click=${() => this._info()}><span>Custom colors</span></qui-badge>
+            </div>
+        </div>
+    </qui-card>
+</div>
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+## License
+
+[Apache 2](http://www.apache.org/licenses/LICENSE-2.0)
---
+++ qui-badge.js
@@ -0,0 +1,156 @@
+import { LitElement, html, css} from 'lit';
+import '@vaadin/icon';
+
+/**
+ * Badge UI Component based on the vaadin theme one
+ * see https://vaadin.com/docs/latest/components/badge
+ */
+export class QuiBadge extends LitElement {
+    static styles = css`
+        [theme~="badge"] {
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+            box-sizing: border-box;
+            padding: 0.4em calc(0.5em + var(--lumo-border-radius-s, 0.25em) / 4);
+            color: var(--lumo-primary-text-color, hsla(211, 63%, 54%, 1.0));
+            background-color: var(--lumo-primary-color-10pct, hsla(214, 100%, 60%, 0.13));
+            border-radius: var(--lumo-border-radius-s, 0.25em);
+            font-family: var(--lumo-font-family, "Open Sans", sans-serif);
+            font-size: var(--lumo-font-size-s, 0.875rem);
+            line-height: 1;
+            font-weight: 500;
+            text-transform: initial;
+            letter-spacing: initial;
+            min-width: calc(var (--lumo-line-height-xs, 1.25) * 1em + 0.45em);
+        }
+        [theme~="success"] {
+            color: var(--lumo-success-text-color, hsla(145, 85%, 25%, 1.0));
+            background-color: var(--lumo-success-color-10pct, hsla(145, 72%, 31%, 0.1));
+        }
+        [theme~="error"] {
+            color: var(--lumo-error-text-color, hsla(3, 89%, 42%, 1.0));
+            background-color: var(--lumo-error-color-10pct, hsla(3, 85%, 49%, 0.1));
+        }
+        [theme~="warning"] {
+            color: var(--lumo-warning-text-color, hsla(30, 89%, 42%, 1.0));
+            background-color: var(--lumo-warning-color-10pct, hsla(30, 100%, 50%, 0.1));
+        }
+        [theme~="contrast"] {
+            color: var(--lumo-contrast-80pct, hsla(214, 41%, 17%, 0.83));
+            background-color: var(--lumo-contrast-5pct, hsla(214, 61%, 25%, 0.05));
+        }
+        [theme~="small"] {
+            font-size: var(--lumo-font-size-xxs, 0.75rem);
+            line-height: 1;
+        }
+        [theme~="tiny"] {
+            font-size: var(--lumo-font-size-xxs, 0.75rem);
+            line-height: 1;
+            padding: 0.2em calc(0.2em + var(--lumo-border-radius-s, 0.25em) / 4);
+        }
+        [theme~="primary"] {
+            color: var(--lumo-primary-contrast-color, hsla(0, 100%, 100%, 1.0));
+            background-color: var(--lumo-primary-color, hsla(211, 63%, 54%, 1.0));
+        }
+        [theme~="successprimary"] {
+            color: var(--lumo-success-contrast-color, hsla(0, 100%, 100%, 1.0));
+            background-color: var(--lumo-success-color, hsla(145, 72%, 30%, 1.0));
+        }
+        [theme~="warningprimary"] {
+            color: var(--lumo-warning-contrast-color, hsla(0, 100%, 100%, 1.0));
+            background-color: var(--lumo-warning-color, hsla(30, 100%, 50%, 1.0));
+        }
+        [theme~="errorprimary"] {
+            color: var(--lumo-error-contrast-color, hsla(0, 100%, 100%, 1.0));
+            background-color: var(--lumo-error-color, hsla(3, 85%, 48%, 1.0));
+        }
+        [theme~="contrastprimary"] {
+            color: var(--lumo-base-color, hsla(0, 100%, 100%, 1.0));
+            background-color: var(--lumo-contrast, hsla(214, 35%, 15%, 1.0));
+        }
+        [theme~="pill"] {
+            --lumo-border-radius-s: 1em;
+        }
+    `;
+
+    static properties = {
+        background: {type: String},
+        color: {type: String},
+        icon: {type: String},
+        level: {type: String},
+        small: {type: Boolean},
+        tiny: {type: Boolean},
+        primary: {type: Boolean},
+        pill: {type: Boolean},
+        clickable: {type: Boolean},
+        _theme: {attribute: false},
+        _style: {attribute: false},
+    };
+
+    constructor(){
+        super();
+        this.icon = null;
+        this.level = null;
+        this.background = null;
+        this.color = null;
+        this.small = false;
+        this.primary = false;
+        this.pill = false;
+        this.clickable = false;
+        
+        this._theme = "badge";
+        this._style = "";
+    }
+
+    connectedCallback() {
+        super.connectedCallback()
+        
+        if(this.level){
+            this._theme = this._theme + " " + this.level;
+        }
+        if(this.primary){
+            if(this.level){
+                this._theme = this._theme + "primary";
+            }else{
+                this._theme = this._theme + " primary";
+            }
+        }
+        
+        if(this.small && !this.tiny){
+            this._theme = this._theme + " small";
+        }
+        if(this.tiny){
+            this._theme = this._theme + " tiny";
+        }
+        
+        if(this.pill){
+            this._theme = this._theme + " pill";
+        }
+        
+        if(this.background){
+            this._style = this._style + "background: " + this.background + ";";
+        }
+        if(this.color){
+            this._style = this._style + "color: " + this.color + ";";
+        }
+        if(this.clickable){
+            this._style = this._style + "cursor: pointer";
+        }
+      }
+      
+    render() {
+        return html`<span theme='${this._theme}' style='${this._style}'>
+                ${this._renderIcon()}
+                <slot></slot>
+            </span>`;
+    }
+
+    _renderIcon(){
+        if(this.icon){
+            return html`<vaadin-icon icon='${this.icon}' style='padding: var(--lumo-space-xs, 0.25rem);'></vaadin-icon>`;
+        }
+    }
+
+}
+customElements.define('qui-badge', QuiBadge);
